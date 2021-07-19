657ffa93bfc4498cd5bc1cff80e49bd1b661e081
tflion <tfleao07@gmail.com>
2021-07-19 16:58:35 -0300

    trocando-formulario-para-class

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,22 +1,36 @@
+import React, { Component } from 'react';
 import './Formulario.css'
 
-const Formulario = () => {
-    return (
-        <form className="form-cadastro">
-            <h1>Gerenciador de Notas</h1>
-            <input
-                type="text"
-                placeholder="Título"
-                className="form-cadastro_input"
-            />
-            <textarea
-                rows={15}
-                placeholder="Escreva sua nota..."
-                className="form-cadastro_input"
-            />
-            <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
-        </form>
-    );
+class Formulario extends Component {
+
+    constructor(){
+        super();
+        this.titulo = "";
+    }
+    handleMudancaTitulo(evento){
+        this.titulo = (evento.target.value)
+    }
+
+
+    render() {
+        return (
+            <form className="form-cadastro">
+                <h1>Gerenciador de Notas</h1>
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input"
+                />
+                <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
+            </form>
+        );
+    }
 }
 
 export default Formulario;
