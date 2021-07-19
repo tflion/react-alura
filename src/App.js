5a8708586453bc3b98c16ff3e95cd44b46c625fb
tflion <tfleao07@gmail.com>
2021-07-19 19:52:52 -0300

    tornando_app_class-e-passando_props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import ListaDeNotas from "./Components/ListaDeNotas";
 
 import './Assets/App.css'
 import './Assets/index.css';
+import { Component } from "react";
 
-const App = () => {
-  return (
-    <section className="conteudo">
-      <Formulario />
-      <ListaDeNotas />
-    </section>
-  );
+class App extends Component {
+  
+
+  criarNota(titulo, texto){
+    console.log(`Uma nova nota foi criada:` + titulo + ` ` + texto)
+  }
+
+  render() {
+
+
+    return (
+      <section className="conteudo">
+        <Formulario criarNota={this.criarNota} />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
